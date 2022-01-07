c7c5ac03c19d87be352a9297597a35538a27bb3e
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2022-01-07 12:38:16 +0100

    Add padding to utility classes

--- src/Views/View1/HowTo.ts
+++ src/Views/View1/HowTo.ts
@@ -12,61 +12,63 @@ const HowTo = ({ order }: HowToProps) => `
     </div>
   </div>
   <div class="stack stack--3">
-    <ol class="timeline">
-      <li class="timeline__item">
-        <div class="timeline__stage">
-          <div class="timeline__badge"></div>
-        </div>
-        <div class="timeline__content">
-          <div class="timeline__heading">Dina kunduppgifter</div>
-          <div class="timeline__description">Hämtas smidigt med BankID [BankID_LOGO]</div>
-        </div>
-      </li>
-      <li class="timeline__item">
-        <div class="timeline__stage">
-          <div class="timeline__badge"></div>
-        </div>
-        <div class="timeline__content">
-          <div class="timeline__heading">Välj leveranssätt</div>
-          <div class="timeline__description">Hemleverans eller hämta hos handlaren.</div>
-        </div>
-      </li>
-      <li class="timeline__item">
-        <div class="timeline__stage">
-          <div class="timeline__badge"></div>
-        </div>
-        <div class="timeline__content">
-          <div class="timeline__heading">Har du inbytesbil?</div>
-          <div class="timeline__description">Få den värderad direkt online.</div>
-        </div>
-      </li>
-      <li class="timeline__item">
-        <div class="timeline__stage">
-          <div class="timeline__badge"></div>
-        </div>
-        <div class="timeline__content">
-          <div class="timeline__heading">Välj betalsätt</div>
-          <div class="timeline__description">Finansieringstillval med låneansökan online [BankID_LOGO] – svar direkt!</div>
-        </div>
-      </li>
-      <li class="timeline__item">
-        <div class="timeline__stage">
-          <div class="timeline__badge"></div>
-        </div>
-        <div class="timeline__content">
-          <div class="timeline__heading">Vill du teckna en försäkring?</div>
-          <div class="timeline__description">Försäkra din nya bil direkt i köpflödet.</div>
-        </div>
-      </li>
-      <li class="timeline__item timeline__item--final">
-        <div class="timeline__stage">
-          <div class="timeline__badge"></div>
-        </div>
+    <div class="p--2">
+      <ol class="timeline">
+        <li class="timeline__item">
+          <div class="timeline__stage">
+            <div class="timeline__badge"></div>
+          </div>
           <div class="timeline__content">
-          <div class="timeline__heading">Sammanställning och orderbekräftelse</div>
-        </div>
-      </li>
-    </ol>
+            <div class="timeline__heading">Dina kunduppgifter</div>
+            <div class="timeline__description">Hämtas smidigt med BankID [BankID_LOGO]</div>
+          </div>
+        </li>
+        <li class="timeline__item">
+          <div class="timeline__stage">
+            <div class="timeline__badge"></div>
+          </div>
+          <div class="timeline__content">
+            <div class="timeline__heading">Välj leveranssätt</div>
+            <div class="timeline__description">Hemleverans eller hämta hos handlaren.</div>
+          </div>
+        </li>
+        <li class="timeline__item">
+          <div class="timeline__stage">
+            <div class="timeline__badge"></div>
+          </div>
+          <div class="timeline__content">
+            <div class="timeline__heading">Har du inbytesbil?</div>
+            <div class="timeline__description">Få den värderad direkt online.</div>
+          </div>
+        </li>
+        <li class="timeline__item">
+          <div class="timeline__stage">
+            <div class="timeline__badge"></div>
+          </div>
+          <div class="timeline__content">
+            <div class="timeline__heading">Välj betalsätt</div>
+            <div class="timeline__description">Finansieringstillval med låneansökan online [BankID_LOGO] – svar direkt!</div>
+          </div>
+        </li>
+        <li class="timeline__item">
+          <div class="timeline__stage">
+            <div class="timeline__badge"></div>
+          </div>
+          <div class="timeline__content">
+            <div class="timeline__heading">Vill du teckna en försäkring?</div>
+            <div class="timeline__description">Försäkra din nya bil direkt i köpflödet.</div>
+          </div>
+        </li>
+        <li class="timeline__item timeline__item--final">
+          <div class="timeline__stage">
+            <div class="timeline__badge"></div>
+          </div>
+            <div class="timeline__content">
+            <div class="timeline__heading">Sammanställning och orderbekräftelse</div>
+          </div>
+        </li>
+      </ol>
+    </div>
   </div>
   <div class="stack stack--3">
     <div class="content">
