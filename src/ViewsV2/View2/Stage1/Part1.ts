c78bbe64f3045971625224cba1bc0eacfee5bd4b
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2022-01-10 14:59:06 +0100

    Update styling

--- src/ViewsV2/View2/Stage1/Part1.ts
+++ src/ViewsV2/View2/Stage1/Part1.ts
@@ -103,11 +103,35 @@ class Part1 {
       <div class="stack stack--3">
         <div class="stack stack--2">
           <div class="input-label">
-            <label for="${EMAIL_INPUT_ID}" class="input-label__label input-label__label--required">E-post</label>
+            <label for="${EMAIL_INPUT_ID}" class="input-label__label input-label__label--is-required">E-post</label>
             <div class="input-label__help">
-              HELP???
+              <button class="input-label__help-btn" title="Vad betyder detta?">
+                <span class="sr-only">Visa hjälp</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  class="icon"
+                  aria-hidden="true"
+                >
+                  <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 14c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6-2.7 6-6 6zm-1.4-3.8h1.9V12H6.6v-1.8zm3.8-3.4c0 1-.5 1.5-1.4 2l-.4.3c-.3.1-.3.2-.3.4v.1H6.7v-.3c0-.6.1-.9.8-1.3l.7-.4c.3-.2.6-.4.6-.8s-.3-.7-.7-.7c-.5 0-.8.3-.8.8v.3H5.7v-.4c0-1.2.8-2 2.3-2 1.5 0 2.4.7 2.4 2z" />
+                </svg>
+                <!--
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  class="icon"
+                  aria-hidden="true"
+                >
+                  <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 14c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6-2.7 6-6 6zm3.5-8.1L9.4 8l2.1 2.1-1.4 1.4L8 9.4l-2.1 2.1-1.4-1.4L6.6 8 4.5 5.9l1.4-1.4L8 6.6l2.1-2.1 1.4 1.4z" />
+                </svg>
+                -->
+              </button>
             </div>
-            <div class="input-label__foldout">Lorem ipsum dolor sit amet.</div>
+            <div class="input-label__foldout">
+              <p><strong>Hur mycket av dina egna pengar vill du lägga?</strong></p>
+              <p>Kontantinsatsen är en del av bilens pris som du betalar med egna pengar. Den behöver vara minst 20% av priset på bilen. Kontantinsatsen betalar du senare i samband med avtalsskrivning hos handlaren.</p>
+              <p>Ifall du har en inbytesbil kan du betala kontantinsatsen med den. Detta kommer du överens om tillsammans med handlaren vid avtalsskrivning. </p>
+            </div>
           </div>
           <input
             type="text"
