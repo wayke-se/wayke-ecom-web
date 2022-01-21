55d13653cfc2ac4d6d7e12e36ef401c8211baf37
Peter <peter@ourstudio.se>
2022-01-21 10:49:25 +0100

    adds more configs

--- src/index-dev.ts
+++ src/index-dev.ts
@@ -1,6 +1,7 @@
 import { config } from '@wayke-se/ecom';
 
 import App from './App';
+import { devConfig } from './devConfig';
 
 window.addEventListener('DOMContentLoaded', (_) => {
   if (!window.process) {
@@ -16,26 +17,7 @@ window.addEventListener('DOMContentLoaded', (_) => {
     },
   };
 
-  const devConfig = {
-    vehicle: {
-      id: 'e821e7c0-a433-46a2-aeba-283e2357133a',
-      title: 'Volvo V70',
-      shortDescription: 'D3 Momentum',
-      price: 138900,
-      imageUrls: [
-        'https://test-cdn.wayketech.se/media/87290e0d-f0c1-48a7-8e08-e4465aa877b5/c8619c99-466c-4b79-956e-5e33b41493ea/1170x',
-        'https://test-cdn.wayketech.se/media/87290e0d-f0c1-48a7-8e08-e4465aa877b5/c8619c99-466c-4b79-956e-5e33b41493ea/1170x',
-        'https://test-cdn.wayketech.se/media/80b2852f-d9fb-4fbe-afa7-81bf2f4b8268/58e4a4bf-b983-4e69-819e-f7ad306999f9/1170x',
-        'https://test-cdn.wayketech.se/media/a854df5b-c152-48df-94d0-58fee0b1ee60/1d96a0a4-4faf-4123-a1cc-618837e80094/1170x',
-      ],
-      modelYear: 2015,
-      milage: 2885,
-      gearBox: 'Automat',
-      fuelType: 'Diesel',
-    },
-  };
-
   config.bind(newConfig);
 
-  new App(devConfig);
+  new App(devConfig.BANKID_TRADE_IN_HOME_DELIVERY);
 });
