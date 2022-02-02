f50b57676d64da11a9bfeded72b31f9dc5aa17d2
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2022-02-02 14:13:12 +0100

    Add styling to detail card

--- src/Views/View2/Stage5Financial/Loan.ts
+++ src/Views/View2/Stage5Financial/Loan.ts
@@ -113,76 +113,132 @@ class Loan {
         <div class="waykeecom-stack waykeecom-stack--3" id="${RESIDUAL_RANGE_NODE}"></div>
 
         <div class="waykeecom-stack waykeecom-stack--3">
-          <div style="border: 5px solid black; padding: 16px;">
-            <div>${this.loan.name}</div>
-            <img style="width: 200px;" src="${this.loan.logo}" />
-            <div>Månadskostnad för lånet: ${prettyNumber(monthlyCost, {
-              postfix: 'kr/mån',
-            })}</div>
-            <p>*Beräknat på ${interest * 100} % ränta (effektivt ${
+          <div class="waykeecom-stepper__break">
+            <div class="waykeecom-shadow-box">
+              <div class="waykeecom-stack waykeecom-stack--3">
+                <div class="waykeecom-stack waykeecom-stack--1">
+                  <h6 class="waykeecom-heading waykeecom-heading--5">${this.loan.name}</h6>
+                </div>
+                <div class="waykeecom-stack waykeecom-stack--1">
+                  <img src="${this.loan.logo}" alt="${
+      this.loan.name
+    } logotyp" class="waykeecom-image waykeecom-image--loan-logo" />
+                </div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--3">
+                <div class="waykeecom-stack waykeecom-stack--1">
+                  <ul class="waykeecom-key-value-list waykeecom-key-value-list--large-value">
+                    ${KeyValueListItem({
+                      key: 'Månadskostnad för lånet',
+                      value: prettyNumber(monthlyCost, {
+                        postfix: 'kr/mån*',
+                      }),
+                    })}
+                  </ul>
+                </div>
+                <div class="waykeecom-stack waykeecom-stack--1">
+                  <div class="waykeecom-disclaimer-text">*Beräknat på ${
+                    interest * 100
+                  } % ränta (effektivt ${
       effectiveInterest * 100
-    } %). Den ränta du får sätts vid avtalskrivning.</p>
-            <h2>Detaljer</h2>
-            <div class="waykeecom-stack waykeecom-stack--1">
-              <ul class="waykeecom-key-value-list">
-                ${KeyValueListItem({
-                  key: 'Kontantinsats',
-                  value: prettyNumber(downPayment, { postfix: 'kr' }),
-                })}
-                ${KeyValueListItem({
-                  key: 'Lån',
-                  value: prettyNumber(creditAmount, { postfix: 'kr' }),
-                })}
-              </ul>
-            </div>
-            <h2>Detaljer</h2>
-            <div class="waykeecom-stack waykeecom-stack--1">
-              <ul class="waykeecom-key-value-list">
-                ${KeyValueListItem({ key: 'Avbetalningsperoid', value: `${duration} mån` })}
+    } %). Den ränta du får sätts vid avtalskrivning.</div>
+                </div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--3">
+                <div class="waykeecom-stack waykeecom-stack--2">
+                  <ul class="waykeecom-key-value-list">
+                    ${KeyValueListItem({
+                      key: 'Kontantinsats',
+                      value: prettyNumber(downPayment, { postfix: 'kr' }),
+                    })}
+                    ${KeyValueListItem({
+                      key: 'Lån',
+                      value: prettyNumber(creditAmount, { postfix: 'kr' }),
+                    })}
+                  </ul>
+                </div>
+                <div class="waykeecom-stack waykeecom-stack--2">
+                  <hr class="waykeecom-separator" />
+                </div>
+                <div class="waykeecom-stack waykeecom-stack--2">
+                  <div class="waykeecom-accordion">
+                    <input type="checkbox" id="finance-details-accordion" class="waykeecom-accordion__checkbox" tabindex="-1" />
+                    <label class="waykeecom-accordion__header" for="finance-details-accordion" tabindex="0" aria-label="Visa detaljer">
+                      <div class="waykeecom-accordion__header-title">Detaljer</div>
+                      <div class="waykeecom-accordion__header-icon">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 16 16"
+                          class="waykeecom-icon"
+                        >
+                          <title>Ikon: vinkel ned</title>
+                          <path d="M6.7 11.4 0 4.6l1.3-1.4L8 10.1l6.7-6.9L16 4.6l-6.7 6.9L8 12.8l-1.3-1.4z" />
+                        </svg>
+                      </div>
+                    </label>
+                    <div class="waykeecom-accordion__body">
+                      <div class="waykeecom-stack waykeecom-stack--2">
+                        <ul class="waykeecom-key-value-list">
+                          ${KeyValueListItem({
+                            key: 'Avbetalningsperoid',
+                            value: `${duration} mån`,
+                          })}
 
-                ${KeyValueListItem({ key: 'Ränta', value: `${interest * 100}%` })}
+                          ${KeyValueListItem({ key: 'Ränta', value: `${interest * 100}%**` })}
 
-                ${KeyValueListItem({ key: 'Effektiv ränta', value: `${effectiveInterest * 100}%` })}
+                          ${KeyValueListItem({
+                            key: 'Effektiv ränta',
+                            value: `${effectiveInterest * 100}%**`,
+                          })}
 
-                ${
-                  setupFee !== undefined &&
-                  KeyValueListItem({
-                    key: 'Uppläggningskostnad',
-                    value: prettyNumber(setupFee, { postfix: 'kr' }),
-                  })
-                }
+                          ${
+                            setupFee !== undefined &&
+                            KeyValueListItem({
+                              key: 'Uppläggningskostnad',
+                              value: prettyNumber(setupFee, { postfix: 'kr' }),
+                            })
+                          }
 
-                ${
-                  administrationFee !== undefined &&
-                  KeyValueListItem({
-                    key: 'Administrativa kostnader',
-                    value: prettyNumber(administrationFee, { postfix: 'kr' }),
-                  })
-                }
-                ${KeyValueListItem({
-                  key: 'Total kreditkostnad',
-                  value: prettyNumber(totalCreditCost, { postfix: 'kr/mån' }),
-                })}
-              </ul>
-            </div>
-            <div class="waykeecom-stack waykeecom-stack--1">
-              <p>*Det här är inte den slutgiltiga offerten. Räntan kan komma att ändras ifall det sker justeringar i initial amorteringsplan, tillägg i utrustning eller andra ändringar som påverkar det initiala prisförslaget. 
-
-              Om marknadsräntan förändras kan månadskostnaden komma att ändras i motsvarande mån. Månadskostnaden kan också komma att påverkas utifrån den kreditriskbedömning som görs efter en kreditupplysning.
-              </p>
-              ${
-                publicUrl
-                  ? `
-                  <a
-                    href="${publicUrl}"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    Mer information om ${this.loan.name}
-                  </a>
-                `
-                  : ''
-              }
+                          ${
+                            administrationFee !== undefined &&
+                            KeyValueListItem({
+                              key: 'Administrativa kostnader',
+                              value: prettyNumber(administrationFee, { postfix: 'kr' }),
+                            })
+                          }
+                          ${KeyValueListItem({
+                            key: 'Total kreditkostnad',
+                            value: prettyNumber(totalCreditCost, { postfix: 'kr/mån' }),
+                          })}
+                        </ul>
+                      </div>
+                      <div class="waykeecom-stack waykeecom-stack--2">
+                        <div class="waykeecom-disclaimer-text">
+                          <p>**Det här är inte den slutgiltiga offerten. Räntan kan komma att ändras ifall det sker justeringar i initial amorteringsplan, tillägg i utrustning eller andra ändringar som påverkar det initiala prisförslaget.</p>
+                          <p>Om marknadsräntan förändras kan månadskostnaden komma att ändras i motsvarande mån. Månadskostnaden kan också komma att påverkas utifrån den kreditriskbedömning som görs efter en kreditupplysning.</p>
+                        </div>
+                      </div>
+                      ${
+                        publicUrl
+                          ? `
+                            <div class="waykeecom-stack waykeecom-stack--2">
+                              <a
+                                href="${publicUrl}"
+                                title="Visa mer information om ${this.loan.name}"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                class="waykeecom-link"
+                              >
+                                Mer information om ${this.loan.name}
+                              </a>
+                            </div>
+                          `
+                          : ''
+                      }
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
