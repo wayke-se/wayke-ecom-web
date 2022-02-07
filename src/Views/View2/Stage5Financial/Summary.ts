085e687ce03d69223a55b0f549d628cf9038a42b
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2022-02-07 09:42:29 +0100

    Add summary base

--- src/Views/View2/Stage5Financial/Summary.ts
+++ src/Views/View2/Stage5Financial/Summary.ts
@@ -13,35 +13,44 @@ const LoanSummary = ({ loan, paymentLookupResponse }: LoanSummaryProps) => {
   const downPayment = paymentLookupResponse.getDownPaymentSpec().current;
   const duration = paymentLookupResponse.getDurationSpec().current;
   const creditAmount = paymentLookupResponse.getCreditAmount();
+
   return `
-    <div class="waykeecom-stack waykeecom-stack--1">
-      Ordern är snart klar, här ser du ditt lånebesked:
+    <div class="waykeecom-stack waykeecom-stack--2">
+      <h4 class="waykeecom-heading waykeecom-heading--4">Billån</h4>
+      <div class="waykeecom-content">
+        <p>Ordern är snart klar, här ser du ditt lånebesked:</p>
+      </div>
     </div>
-    <div class="waykeecom-stack waykeecom-stack--1">
+    <div class="waykeecom-stack waykeecom-stack--2">
       ${Alert({
         tone: 'success',
         children: `
-          <p><b>Grattis! Din låneansökan har beviljats av ${loan.name}.</b></p>
+          <p><strong>Grattis! Din låneansökan har beviljats av ${loan.name}.</strong></p>
           <p> Bilen är inte reserverad ännu. Slutför ordern genom att klicka dig igenom nästkommande steg. Har du frågor under tiden? Kontakta [handlaren] på tel [telefonnummer].</p>
         `,
       })}
     </div>
-    <div class="waykeecom-stack waykeecom-stack--1">
-      <ul class="waykeecom-key-value-list">
-        ${KeyValueListItem({
-          key: 'Kontantinsats',
-          value: prettyNumber(downPayment, { postfix: 'kr' }),
-        })}
-        ${KeyValueListItem({
-          key: 'Lånebelopp',
-          value: prettyNumber(creditAmount, { postfix: 'kr' }),
-        })}
-        ${KeyValueListItem({ key: 'Avbetalningsperoid', value: `${duration} mån` })}
-        ${KeyValueListItem({
-          key: 'Månadskostnad för lånet',
-          value: prettyNumber(downPayment, { postfix: 'kr' }),
-        })}
-      </ul>
+    <div class="waykeecom-stack waykeecom-stack--2">
+      <div class="waykeecom-stack waykeecom-stack--1">
+        <ul class="waykeecom-key-value-list">
+          ${KeyValueListItem({
+            key: 'Kontantinsats',
+            value: prettyNumber(downPayment, { postfix: 'kr' }),
+          })}
+          ${KeyValueListItem({
+            key: 'Lånebelopp',
+            value: prettyNumber(creditAmount, { postfix: 'kr' }),
+          })}
+          ${KeyValueListItem({ key: 'Avbetalningsperoid', value: `${duration} mån` })}
+          ${KeyValueListItem({
+            key: 'Månadskostnad för lånet',
+            value: prettyNumber(downPayment, { postfix: 'kr*' }),
+          })}
+        </ul>
+      </div>
+      <div class="waykeecom-stack waykeecom-stack--1">
+        <div class="waykeecom-disclaimer-text">*Beräknat på X,XX % ränta (effektivt X,XX %). Den ränta du får sätts vid avtalskrivning.</div>
+      </div>
     </div>
   `;
 };
@@ -56,20 +65,24 @@ interface SummaryProps {
 const Summary = ({ paymentType, loan, paymentLookupResponse, changeButtonId }: SummaryProps) => {
   const paymentTypeTitle = PaymentType.Cash ? 'Kontant' : PaymentType.Loan ? 'Billån' : 'Leasing';
   return `
-  <div class="waykeecom-stack waykeecom-stack--1">
-    <ul class="waykeecom-key-value-list">
-      <li class="waykeecom-key-value-list__item">
-        <div class="waykeecom-key-value-list__key">Finansiering</div>
-        <div class="waykeecom-key-value-list__value">${paymentTypeTitle}</div>
-      </li>
-    </ul>
-  </div>
   ${
     paymentType === PaymentType.Loan && loan && paymentLookupResponse
-      ? LoanSummary({ loan, paymentLookupResponse })
-      : ''
+      ? `
+        <div class="waykeecom-stack waykeecom-stack--1">
+          ${LoanSummary({ loan, paymentLookupResponse })}
+        </div>
+      `
+      : `
+        <div class="waykeecom-stack waykeecom-stack--1">
+          <ul class="waykeecom-key-value-list">
+            <li class="waykeecom-key-value-list__item">
+              <div class="waykeecom-key-value-list__key">Finansiering</div>
+              <div class="waykeecom-key-value-list__value">${paymentTypeTitle}</div>
+            </li>
+          </ul>
+        </div>
+      `
   }
-  
   <div class="waykeecom-stack waykeecom-stack--1">
     <div class="waykeecom-align waykeecom-align--end">
       <button id="${changeButtonId}" title="Ändra finansiering" class="waykeecom-link">Ändra</button>
--- src/Views/View3Summary/index.ts
+++ src/Views/View3Summary/index.ts
@@ -16,11 +16,46 @@ class View3Summary {
 
     const contactInformation = order?.getContactInformation();
     this.element.innerHTML = `
-        <div>
-          <h3>Sammanställning</h3>
-          <p><b>Strax klart!</b></p>
-          <p>Granska och godkänn din order för att reservera bilen. Efter det kommer ${contactInformation?.name} att kontakta dig för att slutföra köpet.</p>
-          <p>Köpet blir bindande först när du signerat det definitiva affärsförslaget med ${contactInformation?.name}. Det är även då betalningen sker. </p>
+        <div class="waykeecom-page">
+          <div class="waykeecom-page__body">
+            <div class="waykeecom-container waykeecom-container--narrow">
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <h2 class="waykeecom-heading waykeecom-heading--2">Sammanställning</h2>
+                <div class="waykeecom-content">
+                  <p>
+                    <strong>Strax klart!</strong>
+                  </p>
+                  <p>Granska och godkänn din order för att reservera bilen. Efter det kommer ${contactInformation?.name} att kontakta dig för att slutföra köpet.</p>
+                  <p>Köpet blir bindande först när du signerat det definitiva affärsförslaget med ${contactInformation?.name}. Det är även då betalningen sker. </p>
+                </div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <h3 class="waykeecom-heading waykeecom-heading--3">Din order</h3>
+                <div>ORDER...</div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <h4 class="waykeecom-heading waykeecom-heading--4">Inbytesbil</h4>
+                <div>CONTENT...</div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <h4 class="waykeecom-heading waykeecom-heading--4">Leverans</h4>
+                <div>CONTENT...</div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <h4 class="waykeecom-heading waykeecom-heading--4">Kunduppgifter</h4>
+                <div>CONTENT...</div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <div>BUTTON</div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <div>BUTTON</div>
+              </div>
+              <div class="waykeecom-stack waykeecom-stack--4">
+                <div>DISCLAIMER</div>
+              </div>
+            </div>
+          </div>
         </div>
     `;
   }
