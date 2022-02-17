cf3155bc945432a14481c2ada031f7052e7e2322
Peter <peter@ourstudio.se>
2022-02-17 12:50:02 +0100

    adds suppoert for handling order

--- src/cdn.ts
+++ src/cdn.ts
@@ -9,11 +9,14 @@ declare global {
 window.addEventListener('DOMContentLoaded', (_) => {
   const scriptTag = document.querySelector<HTMLScriptElement>('script[src*="wayke-ecom-web"]');
   if (scriptTag) {
-    const src = scriptTag.src.replace('.js', '.css');
-    const cssFile = document.createElement('link');
-    cssFile.rel = 'stylesheet';
-    cssFile.href = src;
-    document.head.appendChild(cssFile);
+    const shouldInject = !scriptTag.getAttribute('disablecssinjection');
+    if (shouldInject) {
+      const src = scriptTag.src.replace('.js', '.css');
+      const cssFile = document.createElement('link');
+      cssFile.rel = 'stylesheet';
+      cssFile.href = src;
+      document.head.appendChild(cssFile);
+    }
   }
 });
 
--- src/index-dev.ts
+++ src/index-dev.ts
@@ -8,5 +8,9 @@ window.addEventListener('DOMContentLoaded', (_) => {
     },
   };
 
-  new WaykeEcomWeb({ ...devConfig.MULTIPLE_FINANCIAL, config: newConfig });
+  const context = new WaykeEcomWeb({ ...devConfig.MULTIPLE_FINANCIAL, config: newConfig });
+  const button = document.querySelector<HTMLButtonElement>('button');
+  if (button) {
+    button.addEventListener('click', () => context.start());
+  }
 });
