5f6c7f77cefd56ef59fc66b75e7dc78044f0d649
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2022-03-09 13:24:22 +0100

    Add stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -10,9 +10,44 @@ const Template = ({ label, className }) => {
   return `<button class="${className}">${label}</button>`;
 };
 
-export const Primary = Template.bind({});
-
-Primary.args = {
+export const Default = Template.bind({});
+Default.args = {
   label: 'Button',
   className: 'waykeecom-button',
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Button',
+  className: 'waykeecom-button waykeecom-button--primary',
+};
+
+export const Action = Template.bind({});
+Action.args = {
+  label: 'Button',
+  className: 'waykeecom-button waykeecom-button--action',
+};
+
+export const ActionAlt = Template.bind({});
+ActionAlt.args = {
+  label: 'Button',
+  className: 'waykeecom-button waykeecom-button--action-alt',
+};
+
+export const ActionClear = Template.bind({});
+ActionClear.args = {
+  label: 'Button',
+  className: 'waykeecom-button waykeecom-button--action-clear',
+};
+
+export const FullWidth = Template.bind({});
+FullWidth.args = {
+  label: 'Button',
+  className: 'waykeecom-button waykeecom-button--primary  waykeecom-button--full-width',
+};
+
+export const SizeSmall = Template.bind({});
+SizeSmall.args = {
+  label: 'Button',
+  className: 'waykeecom-button waykeecom-button--primary  waykeecom-button--size-small',
 };
