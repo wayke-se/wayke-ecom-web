308875fff08d7ff7e7093ee7e10087af8211cb13
Peter <peter@ourstudio.se>
2022-03-11 14:25:52 +0100

    adds modal close button

--- src/Components/Modal/ModalClose.ts
+++ src/Components/Modal/ModalClose.ts
@@ -1,3 +1,4 @@
+import ButtonClose from '../Button/ButtonClose';
 import HtmlNode from '../Extension/HtmlNode';
 
 interface ModalCloseProps {
@@ -21,24 +22,8 @@ class ModalClose extends HtmlNode {
   render() {
     const { onClose } = this.props;
 
-    this.node.innerHTML = `
-      <button title="Stäng modalen" class="waykeecom-modal__close-btn">
-        <span class="waykeecom-sr-only">Stäng modalen</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          class="waykeecom-icon"
-          aria-hidden="true"
-        >
-          <path d="M9.1 8l5.9 5.9-1.1 1.1L8 9.1 2.1 15 1 13.9 6.9 8 1 2.1 2.1 1 8 6.9 13.9 1 15 2.1 9.1 8z" />
-        </svg>
-      </button>
-    `;
-
     if (onClose) {
-      this.node.querySelectorAll<HTMLButtonElement>('button').forEach((button) => {
-        button.addEventListener('click', () => onClose());
-      });
+      new ButtonClose(this.node, { title: 'Stäng modalen', onClick: () => onClose });
     }
   }
 }
