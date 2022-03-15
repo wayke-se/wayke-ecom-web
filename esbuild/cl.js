a4d62827a444a2abe65149d2a36335f80d1c09c7
Peter <peter@ourstudio.se>
2022-03-15 13:40:09 +0100

    update

--- esbuild/cl.js
+++ esbuild/cl.js
@@ -1,29 +1,49 @@
 const fs = require('fs');
+const esbuild = require('esbuild');
 
 const getFiles = () => fs.readdirSync('component-library/component');
 
+let lock = false;
+fs.watch('component-library/component', async () => {
+  if (!lock) {
+    lock = true;
+    await start();
+    setTimeout(() => {
+      lock = false;
+    }, 1000);
+  }
+});
+
 const { sassPlugin } = require('esbuild-sass-plugin');
 
-require('esbuild').serve(
-  {
-    servedir: 'component-library',
-    port: 8000,
-  },
-  {
-    entryPoints: ['component-library/index.ts'],
-    bundle: true,
-    outdir: 'component-library',
-    loader: {
-      '.js': 'jsx',
-      '.woff': 'file',
-      '.woff2': 'file',
-      '.gif': 'file',
-      '.svg': 'dataurl',
-      '.png': 'dataurl',
-    },
-    plugins: [sassPlugin()],
-    define: {
-      'process.env.COMPONENT_FILES': JSON.stringify(getFiles()),
-    },
+let ref;
+const start = async () => {
+  if (ref) {
+    ref.stop();
   }
-);
+  ref = await esbuild.serve(
+    {
+      servedir: 'component-library',
+      port: 8000,
+    },
+    {
+      entryPoints: ['component-library/index.ts'],
+      bundle: true,
+      outdir: 'component-library',
+      loader: {
+        '.js': 'jsx',
+        '.woff': 'file',
+        '.woff2': 'file',
+        '.gif': 'file',
+        '.svg': 'dataurl',
+        '.png': 'dataurl',
+      },
+      plugins: [sassPlugin()],
+      define: {
+        'process.env.COMPONENT_FILES': JSON.stringify(getFiles()),
+      },
+    }
+  );
+};
+
+start();
