33d852c801208d20f8f6a01a91184ba351658fff
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2022-03-15 09:06:49 +0100

    Add styling to central storage step

--- src/Views/Main/CentralStorage/index.ts
+++ src/Views/Main/CentralStorage/index.ts
@@ -111,21 +111,19 @@ class CentralStorage extends HtmlNode {
     } else if (navigation.stage === index) {
       content.innerHTML = `
       <div class="waykeecom-stack waykeecom-stack--3">
-        <h4 class="waykeecom-heading waykeecom-heading--4 waykeecom-no-margin">Vilken handlare vill du köpa ifrån?</h4>
+        <h4 class="waykeecom-heading waykeecom-heading--4">Vilken handlare vill du köpa ifrån?</h4>
         <div class="waykeecom-content">
-          <p>Detta fordon är en centrallagerbil och finns tillgängligt från flera handlare. Välj den du vill handla ifrån.</p>
+          <p>Detta fordon är en centrallagerbil och finns tillgängligt från flera handlare. Välj den handlare du vill handla ifrån.</p>
         </div>
       </div>
 
-      <div class="waykeecom-stack waykeecom-stack--2">
-        <div class="waykeecom-stack waykeecom-stack--3" id="${CENTRAL_STORAGE_BRANCH_NODE}"></div>
-      </div>
+      <div class="waykeecom-stack waykeecom-stack--3" id="${CENTRAL_STORAGE_BRANCH_NODE}"></div>
 
       ${
         this.requestError
-          ? `<div class="waykeecom-stack waykeecom-stack--2">${Alert({
+          ? `<div class="waykeecom-stack waykeecom-stack--3">${Alert({
               tone: 'error',
-              children: `<p>Ett fel uppstod och det gick inte att välja ovanstående anläggning. Försök igen.</p>`,
+              children: `<p>Ett fel uppstod och det gick inte att välja ovanstående handlare. Försök igen.</p>`,
             })}</div>`
           : ''
       }
@@ -134,16 +132,34 @@ class CentralStorage extends HtmlNode {
     `;
 
       const options: RadioItem[] = dealers.map((dealer) => {
-        const address = `${dealer.location?.address}, ${dealer.location?.city}`;
+        const address =
+          !dealer.location?.address || !dealer.location?.city
+            ? 'Adress saknas'
+            : `${dealer.location?.address}, ${dealer.location?.city}`;
+
         return {
           id: `central-storage-${dealer.id}`,
           value: dealer.id,
           title: dealer.name,
           description: `
             <div class="waykeecom-box">
-              <ul class="waykeecom-unordered-list">
-                <li class="waykeecom-unordered-list__item">${address}</li>
-              </ul>
+              <div class="waykeecom-icon-content">
+                <div class="waykeecom-icon-content__icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    class="waykeecom-icon"
+                    data-icon="Location pin"
+                  >
+                    <path d="M12.2 2C11.1.9 9.6.3 8 .3S4.9.9 3.8 2C2.6 3.2 2 4.7 2 6.3 2 9 5.3 12.9 7.3 15l.7.7.7-.7c2-2.1 5.3-6 5.3-8.7 0-1.6-.6-3.1-1.8-4.3zM8 12.8C5.5 10 4 7.6 4 6.3c0-1.1.4-2.1 1.2-2.9.7-.7 1.7-1.1 2.8-1.1 1.1 0 2.1.4 2.8 1.2.8.7 1.2 1.7 1.2 2.8 0 1.3-1.5 3.7-4 6.5zm2-6.5c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2z" />
+                  </svg>
+                </div>
+                <div class="waykeecom-icon-content__content">
+                  <div class="waykeecom-icon-content__meta">
+                    ${address}
+                  </div>
+                </div>
+              </div>
             </div>`,
         } as RadioItem;
       });
