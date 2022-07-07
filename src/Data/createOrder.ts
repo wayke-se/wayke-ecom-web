dee096a2e5c31e581adb7e47bae6eb6115bb73c8
Peter <peter@ourstudio.se>
2022-07-07 15:15:52 +0200

    Using Math.round around downpayment. Api throws error if downpayment is a float

--- src/Data/createOrder.ts
+++ src/Data/createOrder.ts
@@ -31,7 +31,7 @@ export const createOrder = (store: WaykeStore) => {
 
     paymentBuilder
       .withDuration(duration) // months, only applicable when payment type === PaymentType.Loan
-      .withDownPayment(downPayment); // only applicable when payment type === PaymentType.Loan
+      .withDownPayment(Math.round(downPayment)); // only applicable when payment type === PaymentType.Loan
     if (residual) {
       paymentBuilder.withResidualValue(residual); // only applicable when payment type === PaymentType.Loan
     }
@@ -114,49 +114,3 @@ export const createOrder = (store: WaykeStore) => {
 
   return orders.create(requestBuilder.build());
 };
-
-/*
-const creditAssessment = orders
-  .newCreditAssessment() // Only required for orders using credit assessment
-  .withScoreId('score-id')
-  .withFinancialProductCode('code')
-  .withRecommendation('recommendation')
-  .withDecision('decision')
-  .build();
-
-const payment = orders
-  .newPayment()
-  .withType(aPaymentOption.type)
-  .withDuration(36) // months, only applicable when payment type === PaymentType.Loan
-  .withDownPayment(25000) // only applicable when payment type === PaymentType.Loan
-  .withResidualValue(20000) // only applicable when payment type === PaymentType.Loan
-  .withExternalId('some-id') // only applicable when payment type === PaymentType.Loan
-  .withCreditAssessment(creditAssessment) // only applicable for orders that should use credit assessment
-  .build();
-
-const insurance = orders
-  .newInsurance()
-  .withDrivingRange(2000)
-  .withAddOns(['Add On 1', 'Add On 2']) // optional
-  .build();
-
-const tradein = vehicles
-  .newVehicleTrade()
-  .forVehicle('REGISTRATION-NUMBER')
-  .withMileage(7600)
-  .withCondition(VehicleCondition.VeryGood)
-  .withComment('A very nice car for daily commute!') // optional
-  .build();
-
-const request = orders
-  .newCreateRequest()
-  .forVehicle('VEHICLE-ID-FROM-WAYKE')
-  .withCustomer(customer)
-  .withPayment(payment)
-  .withDeliveryType(aDeliveryOption.type)
-  .withInsurance(insurance) // optional
-  .withTradeIn(tradein) // optional
-  .build();
-
-const response = await orders.create(request);
-*/
