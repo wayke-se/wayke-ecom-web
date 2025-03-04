c10bfce6608381f36cfc761fb17fa58549c49d22
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2025-03-04 15:31:30 +0100

    Update layout of creditor warning

--- src/Views/Main/Financial/index.ts
+++ src/Views/Main/Financial/index.ts
@@ -241,45 +241,6 @@ class Financial extends HtmlNode {
                 : ''
             }
           </div>
-          <div class="waykeecom-box">
-            <div class="waykeecom-creditor-disclaimer">
-              <div class="waykeecom-creditor-disclaimer__icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="59"
-                  height="53"
-                  viewBox="0 0 59 53"
-                  fill="none"
-                >
-                  <title>Varning</title>
-                  <path
-                    d="M29.5255 20.3569V30.5712M29.5255 40.7855H29.551M54.3173 43.3391L33.9335 7.58906C33.4891 6.80308 32.8445 6.14932 32.0657 5.69448C31.2868 5.23964 30.4015 5 29.5001 5C28.5986 5 27.7133 5.23964 26.9344 5.69448C26.1556 6.14932 25.5111 6.80308 25.0666 7.58906L4.68285 43.3391C4.23359 44.1188 3.99802 45.0038 4.00001 45.9042C4.002 46.8046 4.24148 47.6884 4.69417 48.4662C5.14687 49.244 5.79668 49.8879 6.57776 50.3329C7.35883 50.7778 8.2434 51.0079 9.14179 50.9998H49.9093C50.8034 50.9989 51.6815 50.7622 52.4554 50.3136C53.2294 49.865 53.8719 49.2202 54.3186 48.444C54.7652 47.6677 55.0002 46.7874 55 45.8913C54.9998 44.9953 54.7643 44.1151 54.3173 43.3391Z"
-                    stroke="#A43333"
-                    stroke-width="4"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </div>
-              <div class="waykeecom-creditor-disclaimer__body">
-                <div class="waykeecom-creditor-disclaimer__heading">Att låna kostar pengar!</div>
-                <p>
-                  Om du inte kan betala tillbaka skulden i tid riskerar du en betalningsanmärkning. Det kan
-                  leda till svårigheter att få hyra bostad, teckna abonnemang och få nya lån. För stöd, vänd
-                  dig till budget- och skuldrådgivningen i din kommun. Kontaktuppgifter finns på 
-                  <a
-                    href="https://www.konsumentverket.se/"
-                    title="Konsumentverket"
-                    target="_blank"
-                    rel="noopener noreferrer nofollow"
-                    class="waykeecom-link waykeecom-link--no-external-icon"
-                  >
-                    konsumentverket.se
-                  </a>.
-                </p>
-              </div>
-            </div>
-          </div>
           `,
           meta: `<div class="waykeecom-text waykeecom-text--font-bold">${prettyNumber(loanPrice, {
             postfix: loan.unit,
@@ -295,6 +256,47 @@ class Financial extends HtmlNode {
           information: `
             <div class="waykeecom-content waykeecom-content--inherit-size">
               <p class="waykeecom-content__p">Du köper bilen och äger den själv, antingen genom att betala hela bilen med egna medel eller genom ett billån. Du ansvarar själv för försäljningen av bilen och väljer såklart helt fritt när den ska äga rum.</p>
+            </div>
+          `,
+          footer: `
+            <div class="waykeecom-box">
+              <div class="waykeecom-creditor-disclaimer">
+                <div class="waykeecom-creditor-disclaimer__icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="59"
+                    height="53"
+                    viewBox="0 0 59 53"
+                    fill="none"
+                  >
+                    <title>Varning</title>
+                    <path
+                      d="M29.5255 20.3569V30.5712M29.5255 40.7855H29.551M54.3173 43.3391L33.9335 7.58906C33.4891 6.80308 32.8445 6.14932 32.0657 5.69448C31.2868 5.23964 30.4015 5 29.5001 5C28.5986 5 27.7133 5.23964 26.9344 5.69448C26.1556 6.14932 25.5111 6.80308 25.0666 7.58906L4.68285 43.3391C4.23359 44.1188 3.99802 45.0038 4.00001 45.9042C4.002 46.8046 4.24148 47.6884 4.69417 48.4662C5.14687 49.244 5.79668 49.8879 6.57776 50.3329C7.35883 50.7778 8.2434 51.0079 9.14179 50.9998H49.9093C50.8034 50.9989 51.6815 50.7622 52.4554 50.3136C53.2294 49.865 53.8719 49.2202 54.3186 48.444C54.7652 47.6677 55.0002 46.7874 55 45.8913C54.9998 44.9953 54.7643 44.1151 54.3173 43.3391Z"
+                      stroke="#A43333"
+                      stroke-width="4"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <div class="waykeecom-creditor-disclaimer__body">
+                  <div class="waykeecom-creditor-disclaimer__heading">Att låna kostar pengar!</div>
+                  <p>
+                    Om du inte kan betala tillbaka skulden i tid riskerar du en betalningsanmärkning. Det kan
+                    leda till svårigheter att få hyra bostad, teckna abonnemang och få nya lån. För stöd, vänd
+                    dig till budget- och skuldrådgivningen i din kommun. Kontaktuppgifter finns på 
+                    <a
+                      href="https://www.konsumentverket.se/"
+                      title="Konsumentverket"
+                      target="_blank"
+                      rel="noopener noreferrer nofollow"
+                      class="waykeecom-link waykeecom-link--no-external-icon"
+                    >
+                      konsumentverket.se
+                    </a>.
+                  </p>
+                </div>
+              </div>
             </div>
           `,
           options: firstGroupOptions,
